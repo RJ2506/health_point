bb84de692eb789f585a55ceb74ef6f766bd36c9f
rj <rjgayem@gmail.com>
2022-11-23 20:28:25 -0800

    added the request get

--- health/app.py
+++ health/app.py
@@ -93,16 +93,37 @@ def populate_db():
     if result == None:
         hc = Health('Down','Down','Down','Down', time)
     else:
-        res_receiver = requests.get(app_config['eventstore']['url']['receiver'])
-        receiver = res_receiver.json()
-        print(receiver)
+        res_receiver = requests.get(app_config['eventstore']['url']['receiver'], timeout=5)
+        if res_receiver.status_code == 200:
+            receiver = 'Running'
+        else:
+            receiver = 'Down'
 
+        res_storage = requests.get(app_config['eventstore']['url']['storage'], timeout=5)
+        if res_storage.status_code == 200:
+            storage = 'Running'
+        else:
+            storage = 'Down'
         
+        res_processing = requests.get(app_config['eventstore']['url']['processing'], timeout=5)
+        if res_processing.status_code == 200:
+            processing = 'Running'
+        else:
+            processing = 'Down'
+        
+        res_audit = requests.get(app_config['eventstore']['url']['audit'], timeout=5)
+        if res_audit.status_code == 200:
+            audit = 'Running'
+        else:
+            audit = 'Down'
   
-        # bs = Health(
-        # receiver
-        # time
-        # )
+        hc = Health(
+        receiver,
+        storage,
+        processing,
+        audit,
+        time
+        )
         
         # session.add(bs)
     session.add(hc)
