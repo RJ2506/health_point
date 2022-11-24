f8658b22072ebf39c5cde97b928f6e89465fbb9f
rj <rjgayem@gmail.com>
2022-11-23 21:51:53 -0800

    test

--- storage/app.py
+++ storage/app.py
@@ -59,7 +59,8 @@ DB_SESSION = sessionmaker(bind=DB_ENGINE)
 
 def get_healt_check():
     """return 200 status if its running"""
-    return '200'
+    status = '200'
+    return status , 200
 
 
 def get_purchase_item(start_timestamp, end_timestamp):
